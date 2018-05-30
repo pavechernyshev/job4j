cec96ed08e929e9d2e2cb4c26e686c81ce6ce1bf
pavelchernyshev <89119945499@yandex.ru>
2018-05-30 06:21:14 +0300

    1.3. JavDoc. Code convetion. [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate для вычисления арифметичесчких операций + - / *
+* @author Pavel Chernyshev
+* @since 29.05.2018
+* @version 0.1
+*/
 public class Calculate{
+	
+	/**
+	* Конструктор, вывод строки в консоль
+	* @param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
