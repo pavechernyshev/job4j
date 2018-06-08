e4c3b84a22586111638052ee92c34dad3a4f26f8
pavelchernyshev <89119945499@yandex.ru>
2018-06-08 07:09:33 +0300

    6.8. Удаление дубликатов в массиве. [#225]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+/**
+ * @author Pavel Chernyshev (titan100695@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayDuplicate {
+
+    /**
+     * Удаляем дубликаты сохраняя последовательность в массиве.
+     *
+     * @param array
+     * @return
+     */
+    public String[] remove(String[] array) {
+        int unique = array.length;
+        //от начала до предпоследнего элемента.
+        for (int out = 0; out < unique - 1; out++) {
+            //от второго до последнего элемента.
+            for (int in = out + 1; in < unique; in++) {
+                if (array[out] == array[in]) {
+                    //от сравнивоемого до предпоследнего.
+                    for (int indexOfCopyElement = in; indexOfCopyElement < unique - 1; indexOfCopyElement++) {
+                        //переписываем текуищий и последующие элементы на соседние справа.
+                        array[indexOfCopyElement] = array[indexOfCopyElement + 1];
+                    }
+                    unique--;
+                    in--;
+                }
+            }
+        }
+
+        return Arrays.copyOf(array, unique);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Pavel Chernyshev (titan100695@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        String[] array = {"first", "second", "first", "third", "third", "fourth"};
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] result = arrayDuplicate.remove(array);
+        String[] expected = {"first", "second", "third", "fourth"};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate123() {
+        String[] array = {"1", "1", "3", "2", "2", "1"};
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] result = arrayDuplicate.remove(array);
+        String[] expected = {"1", "3", "2"};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenArrayWithoutDuplicatesThenSuchArray() {
+        String[] array = {"1", "4", "3", "2"};
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] result = arrayDuplicate.remove(array);
+        String[] expected = {"1", "4", "3", "2"};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate1236549870() {
+        String[] array = {"1", "1", "2", "1", "3", "3", "1", "6", "6", "5", "3", "4", "5", "9", "1", "8", "7", "9", "8", "0"};
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] result = arrayDuplicate.remove(array);
+        String[] expected = {"1", "2", "3", "6", "5", "4", "9", "8", "7", "0"};
+
+        assertThat(result, is(expected));
+    }
+}
