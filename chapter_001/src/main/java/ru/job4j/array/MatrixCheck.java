7026fc6d91d5e1dc5399fac8e5a4838d96a76549
pavelchernyshev <89119945499@yandex.ru>
2018-06-16 08:27:03 +0300

    7.3 Крестики-нолики на JavaFX [#49777]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -16,4 +16,86 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    /**
+     * Проверяет заполнена ли хотя-бы одна диагональ значениями check.
+     *
+     * @param data
+     * @param check
+     * @return
+     */
+    public boolean hasFillOneDiagonal(boolean[][] data, boolean check) {
+        boolean res = true;
+        boolean isDiagonal = true;
+        boolean isDiagonalReverse = true;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index][index] != check) {
+                isDiagonal = false;
+            }
+
+            if (data[index][data.length - 1 - index] != check) {
+                isDiagonalReverse = false;
+            }
+
+            if (!isDiagonal && !isDiagonalReverse) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public boolean hasFillOneHorizontal(boolean[][] data, boolean check) {
+        boolean res = false;
+        for (boolean[] row: data) {
+            boolean isMonoRow = true;
+            for (boolean item: row) {
+                if (item != check) {
+                    isMonoRow = false;
+                    break;
+                }
+            }
+            if (isMonoRow) {
+                res = true;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    public boolean hasFillOneVertical(boolean[][] data, boolean check) {
+        boolean res = false;
+        int rowLength = data[0].length;
+
+        for (int columnIndex = 0; columnIndex < rowLength; columnIndex++) {
+            boolean isMonoCol = true;
+            for (int rowIndex = 0; rowIndex < data.length; rowIndex++) {
+                if (data[rowIndex][columnIndex] != check) {
+                    isMonoCol = false;
+                }
+            }
+            if (isMonoCol) {
+                res = true;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public boolean hasFillOneHorizontalOrVertical(boolean[][] data, boolean check) {
+        boolean res = false;
+
+        MatrixCheck matrixCheck = new MatrixCheck();
+
+        if (matrixCheck.hasFillOneHorizontal(data, check)) {
+            res = true;
+        } else {
+            if (matrixCheck.hasFillOneVertical(data, check)) {
+                res = true;
+            }
+        }
+
+        return res;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -59,4 +59,169 @@ public class MatrixCheckTest {
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
+
+    @Test
+    public void whenDataHasFillOneDiagonalByTrueCheckTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, true, true},
+                {true, false, true}
+        };
+        boolean result = check.hasFillOneDiagonal(input, true);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataHasFillOneDiagonalByTrueCheckFalseThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, true, true},
+                {true, false, true}
+        };
+        boolean result = check.hasFillOneDiagonal(input, false);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataHasFillOneDiagonalByFalseCheckFalseThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, true},
+                {false, false, true}
+        };
+        boolean result = check.hasFillOneDiagonal(input, false);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasFillOneHorizontalNotHaveCheckFalseThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true},
+                {false, false, true},
+        };
+        boolean result = check.hasFillOneHorizontal(input, false);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenHasFillOneHorizontalHaveCheckFalseThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, false},
+                {false, false, true}
+        };
+        boolean result = check.hasFillOneHorizontal(input, false);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasFillOneHorizontalHaveCheckTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true},
+                {false, false, true},
+                {false, false, true}
+        };
+        boolean result = check.hasFillOneHorizontal(input, true);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasFillOneVerticalHaveCheckTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {false, true, true},
+                {false, false, true},
+                {true, false, true}
+        };
+        boolean result = check.hasFillOneVertical(input, true);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasFillOneVerticalHaveCheckFalseThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {false, true, false},
+                {false, false, true},
+                {false, false, true}
+        };
+        boolean result = check.hasFillOneVertical(input, false);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasFillOneVerticalNotHaveCheckFalseThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true},
+                {false, false, true},
+        };
+        boolean result = check.hasFillOneVertical(input, false);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenHasFillOneVerticalNotHaveCheckTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, true},
+        };
+        boolean result = check.hasFillOneVertical(input, true);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenHasFillOneHorizontalOrVerticalNotHaveCheckTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, true},
+                {true, false, true},
+        };
+        boolean result = check.hasFillOneHorizontalOrVertical(input, true);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenHasFillOneHorizontalOrVerticalNotHaveCheckFalseThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, true},
+                {true, false, true},
+        };
+        boolean result = check.hasFillOneHorizontalOrVertical(input, false);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenHasFillOneHorizontalOrVerticalHaveCheckFalseThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, false},
+                {true, false, true},
+        };
+        boolean result = check.hasFillOneHorizontalOrVertical(input, false);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasFillOneHorizontalOrVerticalHaveCheckTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, false},
+                {true, true, true},
+        };
+        boolean result = check.hasFillOneHorizontalOrVertical(input, true);
+        assertThat(result, is(true));
+    }
 }
