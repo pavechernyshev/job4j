9e44f7470ffc4391677d4431f275afdd4f36751f
pavelchernyshev <89119945499@yandex.ru>
2018-07-19 08:38:20 +0300

    Каркас шахматной доски [#792] Knight

--- chapter_002/src/main/java/ru/job4j/chess/firuges/black/KnightBlack.java
+++ chapter_002/src/main/java/ru/job4j/chess/firuges/black/KnightBlack.java
@@ -2,6 +2,7 @@ package ru.job4j.chess.firuges.black;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+import ru.job4j.chess.firuges.exceptions.ImpossibleMoveException;
 
 /**
  *
@@ -23,6 +24,19 @@ public class KnightBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
+        if (source.x == dest.x + 1 || source.x == dest.x - 1) {
+            int stepY = source.y > dest.y ? source.y - dest.y : dest.y - source.y;
+            if (stepY != 2) {
+                throw new ImpossibleMoveException("Конь ходит буквой Г");
+            }
+        } else if (source.y == dest.y + 1 || source.y == dest.y - 1) {
+            int stepX = source.x > dest.x ? source.x - dest.x : dest.x - source.x;
+            if (stepX != 2) {
+                throw new ImpossibleMoveException("Конь ходит буквой Г");
+            }
+        } else {
+            throw new ImpossibleMoveException("Конь ходит буквой Г");
+        }
         return new Cell[] {dest };
     }
 
