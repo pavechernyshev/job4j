f94d87fb584d0fa3a55ad016ab12173449d8dea6
pavelchernyshev <89119945499@yandex.ru>
2018-07-19 08:19:58 +0300

    Каркас шахматной доски [#792] Rook

--- chapter_002/src/main/java/ru/job4j/chess/firuges/black/RookBlack.java
+++ chapter_002/src/main/java/ru/job4j/chess/firuges/black/RookBlack.java
@@ -2,6 +2,7 @@ package ru.job4j.chess.firuges.black;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+import ru.job4j.chess.firuges.exceptions.ImpossibleMoveException;
 
 /**
  *
@@ -22,8 +23,35 @@ public class RookBlack implements Figure {
     }
 
     @Override
-    public Cell[] way(Cell source, Cell dest) {
-        return new Cell[] {dest };
+    public Cell[] way(Cell source, Cell dest) throws ImpossibleMoveException {
+        int range = range(source, dest);
+        if (dest.x != source.x && dest.y != source.y) {
+            throw new ImpossibleMoveException("Ладья ходит по прямой");
+        }
+        if (range == -1) {
+            throw new ImpossibleMoveException("Не удалось выяснить растояние хода для ладьи");
+        }
+        Cell[] steps = new Cell[range];
+        int deltaX = source.x == dest.x ? 0 : source.x > dest.x ? -1 : 1;
+        int deltaY = source.y == dest.y ? 0 : source.y > dest.y ? -1 : 1;
+        int x = source.x;
+        int y = source.y;
+        for (int index = 0; index < range; index++) {
+            x += deltaX;
+            y += deltaY;
+            steps[index] = Cell.values()[8 * x + y];
+        }
+        return steps;
+    }
+
+    private int range(Cell source, Cell dest) {
+        int res = -1;
+        if (source.x != dest.x) {
+            res = source.x > dest.x ? source.x - dest.x : dest.x - source.x;
+        } else if (source.y != dest.y) {
+            res = source.y > dest.y ? source.y - dest.y : dest.y - source.y;
+        }
+        return res;
     }
 
     @Override
