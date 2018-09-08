de173196b7ea9b1304acb1d4b471b0124b31bed9
pavelchernyshev <89119945499@yandex.ru>
2018-09-08 10:05:50 +0300

    3.5 Отсортировать департаменты [#34352]

---
+++ chapter_003/src/main/java/ru/job4j/exam/DepartmentsList.java
@@ -0,0 +1,53 @@
+package ru.job4j.exam;
+
+import java.util.*;
+
+import static java.lang.String.format;
+
+public class DepartmentsList {
+
+    class DepartmentsComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
+        }
+    }
+
+    class DepartmentsResortComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            String unit1 = o1.split(format("\\%s", separator))[0];
+            String unit2 = o2.split(format("\\%s", separator))[0];
+            return unit2.compareTo(unit1);
+        }
+    }
+
+    private String separator = "\\";
+    private List<String> departments = new LinkedList<>();
+
+    public void add(String codes) {
+        this.departments.add(codes);
+
+        String[] units = codes.split(format("\\%s", separator));
+        if (units.length > 1) {
+            String check = units[0];
+            for (int i = 0; i < units.length - 1; i++) {
+                if (!this.departments.contains(check)) {
+                    this.departments.add(check);
+                }
+                check = format("%s%s%s", check, separator, units[i + 1]);
+            }
+        }
+    }
+
+    public List<String> sort() {
+        departments.sort(new DepartmentsComparator());
+        return this.departments;
+    }
+
+    public List<String> resort() {
+        departments.sort(new DepartmentsResortComparator().thenComparing(new DepartmentsComparator()));
+        return this.departments;
+    }
+
+}
---
+++ chapter_003/src/test/java/ru/job4j/exam/DepartmentsListTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.exam;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+public class DepartmentsListTest {
+    private DepartmentsList dl = new DepartmentsList();
+
+    @Before
+    public void fill() {
+        this.dl.add("K1\\SK1");
+        this.dl.add("K1\\SK2");
+        this.dl.add("K1\\SK1\\SSK2");
+        this.dl.add("K1\\SK1\\SSK1");
+        this.dl.add("K2");
+        this.dl.add("K2\\SK1\\SSK1");
+        this.dl.add("K2\\SK1\\SSK2");
+    }
+
+    @Test
+    public void whenSortedListTheSortList() {
+        List<String> expect = Arrays.asList(
+                "K1",
+                "K1\\SK1",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK2",
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2"
+        );
+        assertThat(dl.sort(), is(expect));
+    }
+
+    @Test
+    public void whenResortedListTheResortList() {
+        List<String> expect = Arrays.asList(
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2",
+                "K1",
+                "K1\\SK1",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK2"
+        );
+        assertThat(dl.resort(), is(expect));
+    }
+}
