31269fdb5ca8e54f5c5158600e4e7d9f9fa6089d
pavelchernyshev <89119945499@yandex.ru>
2018-09-14 23:03:21 +0300

    Отсортировать департаменты [#34352] добавление класса Org

--- chapter_003/src/main/java/ru/job4j/exam/DepartmentsList.java
+++ chapter_003/src/main/java/ru/job4j/exam/DepartmentsList.java
@@ -6,41 +6,88 @@ import static java.lang.String.format;
 
 public class DepartmentsList {
 
+    class Org implements Comparable<Org> {
+        private String parent;
+        private String code;
+
+        public String getParent() {
+            return parent;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public Org(String code) {
+            this.code = code;
+            this.parent = this.code.split("\\\\")[0];
+        }
+
+        @Override
+        public int compareTo(Org o) {
+            return this.code.compareTo(o.getCode());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Org org = (Org) o;
+            return Objects.equals(code, org.code);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(code);
+        }
+
+        @Override
+        public String toString() {
+            return this.code;
+        }
+    }
+
     public String[] parse(String[] departments, boolean resort) {
-        Set<String> result;
+        Set<Org> orgs;
+        List<String> result = new ArrayList<>();
         if (resort) {
-            result = new TreeSet<>(new DepartmentsResortComparator());
+            orgs = new TreeSet<>(new DepartmentsResortComparator());
         } else {
-            result = new TreeSet<>();
+            orgs = new TreeSet<>();
         }
         for (String codes: departments) {
             String[] units = codes.split("\\\\");
             if (units.length > 0) {
                 String lvl = units[0];
-                result.add(lvl);
+                orgs.add(new Org(lvl));
                 for (int i = 1; i < units.length; i++) {
                     lvl = format("%s\\%s", lvl, units[i]);
-                    result.add(lvl);
+                    orgs.add(new Org(lvl));
                 }
             }
+        }
+        for (Org org: orgs) {
+            result.add(org.getCode());
         }
         return result.toArray(new String[0]);
     }
 
-    class DepartmentsResortComparator implements Comparator<String> {
+    class DepartmentsResortComparator implements Comparator<Org> {
         @Override
-        public int compare(String o1, String o2) {
-            String unit1 = o1.split("\\\\")[0];
-            String unit2 = o2.split("\\\\")[0];
-            int res = unit2.compareTo(unit1);
-            if (res == 0) {
-                res = Integer.compare(o1.length(), o2.length());
+        public int compare(Org o1, Org o2) {
+            int result = o2.getParent().compareTo(o1.getParent());
+            if (result == 0) {
+                result = Integer.compare(o1.getCode().length(), o2.getCode().length());
             }
-            if (res == 0) {
-                res = o2.compareTo(o1);
+            if (result == 0) {
+                result = o2.getCode().compareTo(o1.getCode());
             }
-            return res;
+
+            return result;
         }
     }
-
 }
