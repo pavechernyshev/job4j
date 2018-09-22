b6a1ee4a00f78116ae71c690b1a63276bc416032
pavelchernyshev <89119945499@yandex.ru>
2018-09-22 08:04:06 +0300

    2.1.1 | 5.1.1. Итератор для двухмерного массива int[][] [#9539]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/JaggedArrayIterator.java
@@ -0,0 +1,36 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class JaggedArrayIterator implements Iterator {
+    private final int[][] array;
+    private final boolean isEmpty;
+    private int first = 0;
+    private int second = 0;
+
+    public JaggedArrayIterator(int[][] array) {
+        this.array = array;
+        this.isEmpty = !hasNext();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return array.length > first && array[first].length > second;
+    }
+
+    @Override
+    public Object next() {
+        if (isEmpty) {
+            throw new NoSuchElementException();
+        }
+        int res = array[first][second];
+        if (array[first].length - 1 > second) {
+            second++;
+        } else {
+            second = 0;
+            first++;
+        }
+        return res;
+    }
+}
---
+++ chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,8 @@
+package ru.job4j.iterator;
+
+public class MatrixIterator extends JaggedArrayIterator {
+
+    public MatrixIterator(int[][] array) {
+        super(array);
+    }
+}
