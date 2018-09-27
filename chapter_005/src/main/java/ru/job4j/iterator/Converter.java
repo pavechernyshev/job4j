98970eb739014d5e8b31736089760e234fbab04b
pavelchernyshev <89119945499@yandex.ru>
2018-09-27 21:16:45 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#72555] Добавил тест и доработал код

--- chapter_005/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_005/src/main/java/ru/job4j/iterator/Converter.java
@@ -9,7 +9,20 @@ public class Converter {
             Iterator<Integer> sub = it.next();
             @Override
             public boolean hasNext() {
-                return sub.hasNext();
+                boolean result = false;
+                while (true) {
+                    if (!sub.hasNext() && !it.hasNext()) {
+                        break;
+                    }
+                    if (!sub.hasNext() && it.hasNext()) {
+                        sub = it.next();
+                    }
+                    if (sub.hasNext()) {
+                        result = true;
+                        break;
+                    }
+                }
+                return result;
             }
 
             @Override
@@ -17,11 +30,7 @@ public class Converter {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                Integer res = sub.next();
-                if (!hasNext() && it.hasNext()) {
-                    sub = it.next();
-                }
-                return res;
+                return sub.next();
             }
         };
     }
