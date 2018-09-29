dca708bb9bba3f59e0f187db9fa3763992f8eac0
pavelchernyshev <89119945499@yandex.ru>
2018-09-29 07:32:51 +0300

    Base abstract class

---
+++ chapter_005/src/main/java/ru/job4j/generics/Base.java
@@ -0,0 +1,18 @@
+package ru.job4j.generics;
+
+/***
+ * @author Павел Чернышев
+ * @version $Id$
+ * @since 0.1
+ */
+public abstract class Base {
+    private final String id;
+
+    protected Base(final String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
