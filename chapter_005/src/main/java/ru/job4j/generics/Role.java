a6e304291a59cb71315cf73230b650dd4f270341
pavelchernyshev <89119945499@yandex.ru>
2018-09-29 07:44:02 +0300

    Role and user classes

---
+++ chapter_005/src/main/java/ru/job4j/generics/Role.java
@@ -0,0 +1,12 @@
+package ru.job4j.generics;
+
+/***
+ * @author Павел Чернышев (pavel.chernyshev.work@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Role extends Base{
+    public Role(String id) {
+        super(id);
+    }
+}
---
+++ chapter_005/src/main/java/ru/job4j/generics/User.java
@@ -0,0 +1,12 @@
+package ru.job4j.generics;
+
+/***
+ * @author Павел Чернышев (pavel.chernyshev.work@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class User extends Base{
+    public User(String id) {
+        super(id);
+    }
+}
