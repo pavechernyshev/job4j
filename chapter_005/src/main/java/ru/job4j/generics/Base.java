b1251778ee26579e33bb5ac683a57ab2d0af6300
pavelchernyshev <89119945499@yandex.ru>
2018-10-01 22:43:51 +0300

    equals and hash code for Base and extends classes

--- chapter_005/src/main/java/ru/job4j/generics/Base.java
+++ chapter_005/src/main/java/ru/job4j/generics/Base.java
@@ -1,5 +1,7 @@
 package ru.job4j.generics;
 
+import java.util.Objects;
+
 /***
  * @author Павел Чернышев
  * @version $Id$
@@ -14,5 +16,22 @@ public abstract class Base {
 
     public String getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Base base = (Base) o;
+        return Objects.equals(id, base.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
