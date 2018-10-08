12c10f4e8cdbb1de47317072725835ab3e1c6cad
pavelchernyshev <89119945499@yandex.ru>
2018-10-08 21:23:31 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#73861] Использую алгоритм черепахи и зайца

--- chapter_005/src/main/java/ru/job4j/list/Helper.java
+++ chapter_005/src/main/java/ru/job4j/list/Helper.java
@@ -7,38 +7,23 @@ package ru.job4j.list;
  */
 public class Helper {
     public boolean hasCycle(Node node) {
-        Node startNode = node;
-        boolean notChecked = true;
-        boolean res = false;
-        int counter = 0;
-        while (notChecked) {
-            node = node.next;
-            if (node == null) {
-                notChecked = false;
-            } else if (hasNode(startNode, node, ++counter)) {
-                notChecked = false;
-                res = true;
-            }
-        }
-        return res;
-    }
-
-    /***
-     * Check Node in diapason first to count
-     * @param first Node start
-     * @param check Node whose check
-     * @param count count of use method next
-     * @return boolean has check Node in diapason
-     */
-    private boolean hasNode(Node first, Node check, int count) {
-        boolean res = false;
-        for (int i = 0; i < count; i++) {
-            if (first == check) {
-                res = true;
+        Node turtle = node;
+        Node hare = node.next;
+        boolean hasCycle = false;
+        while (true) {
+            if (turtle == null || hare == null) {
                 break;
             }
-            first = first.next;
+            if (turtle == hare) {
+                hasCycle = true;
+                break;
+            }
+            turtle = turtle.next;
+            hare = hare.next;
+            if (hare != null) {
+                hare = hare.next;
+            }
         }
-        return res;
+        return hasCycle;
     }
 }
