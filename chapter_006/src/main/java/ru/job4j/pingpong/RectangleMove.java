f8eb07e196caf5a36aea40537b03974e4e49a6c9
pavelchernyshev <89119945499@yandex.ru>
2018-11-26 20:11:55 +0300

    1. Пин-понг. [#1016] Переводим поля класса в final

--- chapter_006/src/main/java/ru/job4j/pingpong/RectangleMove.java
+++ chapter_006/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -4,47 +4,45 @@ import javafx.scene.shape.Rectangle;
 
 public class RectangleMove implements Runnable {
     private final Rectangle rect;
-    private HorizontalVector hv;
-    private VerticalVector vv;
-    private int speed;
-    private int limitX;
-    private int limitY;
+    private final Vector vector;
+    private final int speed;
+    private final int limitX;
+    private final int limitY;
 
     public RectangleMove(Rectangle rect, HorizontalVector hv, VerticalVector vv, int speed, int limitX, int limitY) {
         this.rect = rect;
-        this.hv = hv;
-        this.vv = vv;
         this.speed = speed;
         this.limitX = limitX;
         this.limitY = limitY;
+        this.vector = new Vector(hv, vv);
     }
 
     private void move() {
         directControl();
         double xPos = rect.getX();
         double yPos = rect.getY();
-        if (hv == HorizontalVector.Left) {
+        if (vector.getHv() == HorizontalVector.Left) {
             rect.setX(xPos - speed);
-        } else if (hv == HorizontalVector.Right) {
+        } else if (vector.getHv() == HorizontalVector.Right) {
             rect.setX(xPos + speed);
         }
-        if (this.vv == VerticalVector.Up) {
+        if (vector.getVv() == VerticalVector.Up) {
             rect.setY(yPos - speed);
-        } else if (this.vv == VerticalVector.Down) {
+        } else if (vector.getVv() == VerticalVector.Down) {
             rect.setY(yPos + speed);
         }
     }
 
     private void directControl() {
-        if (this.hv == HorizontalVector.Left && rect.getX() <= 0) {
-            this.hv = HorizontalVector.Right;
-        } else if (this.hv == HorizontalVector.Right && rect.getX() >= limitX) {
-            this.hv = HorizontalVector.Left;
+        if (vector.getHv() == HorizontalVector.Left && rect.getX() <= 0) {
+            vector.setHv(HorizontalVector.Right);
+        } else if (vector.getHv() == HorizontalVector.Right && rect.getX() >= limitX) {
+            vector.setHv(HorizontalVector.Left);
         }
-        if (this.vv == VerticalVector.Up && rect.getY() <= 0) {
-            this.vv = VerticalVector.Down;
-        } else if (this.vv == VerticalVector.Down && rect.getY() >= limitY) {
-            this.vv = VerticalVector.Up;
+        if (vector.getVv() == VerticalVector.Up && rect.getY() <= 0) {
+            vector.setVv(VerticalVector.Down);
+        } else if (vector.getVv() == VerticalVector.Down && rect.getY() >= limitY) {
+            vector.setVv(VerticalVector.Up);
         }
     }
 
@@ -57,6 +55,31 @@ public class RectangleMove implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+    class Vector {
+        private HorizontalVector hv;
+        private VerticalVector vv;
+
+        Vector(HorizontalVector hv, VerticalVector vv) {
+            this.hv = hv;
+            this.vv = vv;
+        }
+
+        public HorizontalVector getHv() {
+            return hv;
+        }
+
+        public VerticalVector getVv() {
+            return this.vv;
+        }
+
+        public void setHv(HorizontalVector hv) {
+            this.hv = hv;
+        }
+
+        public void setVv(VerticalVector vv) {
+            this.vv = vv;
         }
     }
 
