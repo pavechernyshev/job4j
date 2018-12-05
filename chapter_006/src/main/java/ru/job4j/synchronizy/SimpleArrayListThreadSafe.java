ebd443d1a7d3c634b8a78c6667808a025a971d8e
pavelchernyshev <89119945499@yandex.ru>
2018-12-05 22:08:27 +0300

    Не рабочая версия

---
+++ chapter_006/src/main/java/ru/job4j/synchronizy/SimpleArrayListThreadSafe.java
@@ -0,0 +1,38 @@
+package ru.job4j.synchronizy;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import ru.job4j.list.SimpleArrayList;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+@ThreadSafe
+public class SimpleArrayListThreadSafe<E> implements Iterable<E> {
+    @GuardedBy("this")
+    SimpleArrayList<E> sal;
+
+    SimpleArrayListThreadSafe(SimpleArrayList<E> sal) {
+        this.sal = sal;
+    }
+
+    public synchronized void add(E data) {
+        sal.add(data);
+    }
+
+    @Override
+    public synchronized Iterator<E> iterator() {
+        return copy(this.sal).iterator();
+    }
+
+    private synchronized SimpleArrayList<E> copy(SimpleArrayList<E> sal) {
+        /* не работчая версия */
+        SimpleArrayList<E> res = new SimpleArrayList<>();
+        for (E item: sal) {
+            res.add(item);
+        }
+        return res;
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4j/synchronizy/SimpleArrayListThreadSafeTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.synchronizy;
+
+import org.hamcrest.core.Is;
+import org.hamcrest.core.Is.*;
+import org.junit.Ignore;
+import org.junit.Test;
+import ru.job4j.list.SimpleArrayList;
+
+
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class SimpleArrayListThreadSafeTest {
+
+    @Ignore
+    @Test
+    public void whenTestIterator() {
+        SimpleArrayList<Integer> sal = new SimpleArrayList<>();
+        sal.add(1);
+        sal.add(2);
+        sal.add(3);
+        SimpleArrayListThreadSafe<Integer> salts = new SimpleArrayListThreadSafe<>(sal);
+        Iterator i = salts.iterator();
+        salts.add(4);
+        assertTrue(i.hasNext());
+        assertThat(i.next(), Is.is(1));
+        assertTrue(i.hasNext());
+        assertThat(i.next(), Is.is(2));
+        assertTrue(i.hasNext());
+        assertThat(i.next(), Is.is(3));
+        assertFalse(i.hasNext());
+    }
+
+}
