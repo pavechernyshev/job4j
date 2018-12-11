6277015ce2b05376275be2a6b83104e7d2a8e2a5
pavelchernyshev <89119945499@yandex.ru>
2018-12-11 07:04:33 +0300

    fixes variable name

--- chapter_006/src/main/java/ru/job4j/jmm/JmmProblem.java
+++ chapter_006/src/main/java/ru/job4j/jmm/JmmProblem.java
@@ -6,12 +6,12 @@ public class JmmProblem {
         Variable var = new Variable(0);
         Thread first = new Thread(new PrintVariable("first", var));
         Thread second = new Thread(new PrintVariable("second", var));
-        Thread theard = new Thread(new PrintVariable("theard", var));
+        Thread thread = new Thread(new PrintVariable("thread", var));
         Thread fourth = new Thread(new PrintVariable("fourth", var));
         Thread five = new Thread(new PrintVariable("five", var));
         first.start();
         second.start();
-        theard.start();
+        thread.start();
         fourth.start();
         five.start();
     }
