204fd6d856cc85ffa7ca5d2c53b97fd4b1a3e563
pavelchernyshev <89119945499@yandex.ru>
2019-01-09 22:18:05 +0300

    2. Удаление запрещенных слов [#859]

--- chapter_007/src/main/java/ru/job4j/inout/InputStreamHelper.java
+++ chapter_007/src/main/java/ru/job4j/inout/InputStreamHelper.java
@@ -1,10 +1,14 @@
 package ru.job4j.inout;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class InputStreamHelper {
-    boolean isHasEvenNumber(InputStream in) throws IOException {
+    public boolean isHasEvenNumber(InputStream in) throws IOException {
         boolean res = false;
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in))) {
             // чтение построчно
@@ -16,5 +20,23 @@ public class InputStreamHelper {
             }
         }
         return res;
+    }
+
+    public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
+        OutputStream outputStream = new BufferedOutputStream(out);
+        List<String> abuseList =  Arrays.asList(abuse);
+        try (Scanner scanner = new Scanner(in)) {
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                if (!abuseList.contains(word)) {
+                    if (scanner.hasNext()) {
+                        word += " ";
+                    }
+                    out.write(word.getBytes(Charset.forName("UTF-8")));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- chapter_007/src/test/java/ru/job4j/inout/InputStreamHelperTest.java
+++ chapter_007/src/test/java/ru/job4j/inout/InputStreamHelperTest.java
@@ -1,11 +1,9 @@
 package ru.job4j.inout;
 
+import org.hamcrest.core.Is;
 import org.junit.Test;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 
 import static org.junit.Assert.*;
@@ -36,4 +34,39 @@ public class InputStreamHelperTest {
         }
     }
 
+    @Test
+    public void whenDropAbusesDropNothing() {
+        String[] abuses = new String[0];
+        String stringWithoutAbuses = "It is sting without abuses";
+        String expectedString = stringWithoutAbuses;
+        InputStream inputStreamWithAbuses = new ByteArrayInputStream(stringWithoutAbuses.getBytes(Charset.forName("UTF-8")));
+        OutputStream result = new ByteArrayOutputStream(expectedString.length());
+        OutputStream expected = new ByteArrayOutputStream(expectedString.length());
+        try {
+            expected.write(expectedString.getBytes(Charset.forName("UTF-8")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        inputStreamHelper.dropAbuses(inputStreamWithAbuses, result, abuses);
+        assertThat(((ByteArrayOutputStream) result).toString(), Is.is(((ByteArrayOutputStream) expected).toString()));
+    }
+
+    @Test
+    public void whenDropAbusesDropHelloAndBye() {
+        String[] abuses = new String[2];
+        abuses[0] = "Hello";
+        abuses[1] = "bye";
+        String stringWithAbuses = "Hello it is sting with abuses bye";
+        String expectedString = "it is sting with abuses ";
+        InputStream inputStreamWithAbuses = new ByteArrayInputStream(stringWithAbuses.getBytes(Charset.forName("UTF-8")));
+        OutputStream result = new ByteArrayOutputStream(expectedString.length());
+        OutputStream expected = new ByteArrayOutputStream(expectedString.length());
+        try {
+            expected.write(expectedString.getBytes(Charset.forName("UTF-8")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        inputStreamHelper.dropAbuses(inputStreamWithAbuses, result, abuses);
+        assertThat(((ByteArrayOutputStream) result).toString(), Is.is(((ByteArrayOutputStream) expected).toString()));
+    }
 }
