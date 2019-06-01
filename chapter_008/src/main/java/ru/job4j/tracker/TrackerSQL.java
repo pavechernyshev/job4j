f23d7693807ab5f714d08880373739b016784538
pavelchernyshev <89119945499@yandex.ru>
2019-06-01 12:31:02 +0300

    4.2. Трекер SQL [#1734] реализация методов и тесты

--- chapter_008/src/main/java/ru/job4j/tracker/TrackerSQL.java
+++ chapter_008/src/main/java/ru/job4j/tracker/TrackerSQL.java
@@ -1,13 +1,18 @@
 package ru.job4j.tracker;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.job4j.log.UsageLog4j2;
+
+import java.sql.*;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.Properties;
 
 public class TrackerSQL implements ITracker, AutoCloseable {
-
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j2.class.getName());
     private Connection connection;
 
     public boolean init() {
@@ -38,31 +43,115 @@ public class TrackerSQL implements ITracker, AutoCloseable {
 
     @Override
     public Item add(Item item) {
-        return null;
+        String sql = "insert into items (name, description, created) values (?, ?, ?)";
+        try (PreparedStatement ps = this.connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, item.getName());
+            ps.setString(2, item.getDesc());
+            Timestamp created;
+            if (item.getCreated() != 0) {
+                created = new Timestamp(item.getCreated());
+            } else {
+                created = new Timestamp(new Date().getTime());
+            }
+            ps.setTimestamp(3, created);
+            ps.execute();
+            ResultSet generatedKeys = ps.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                int id = generatedKeys.getInt(1);
+                item.setId(String.valueOf(id));
+            }
+        } catch (SQLException e) {
+            LOG.catching(e);
+        }
+
+        return item;
     }
 
     @Override
     public boolean replace(String id, Item item) {
+        String sql = "update items set name=?, description=?, created=? where id=?";
+        try (PreparedStatement ps = this.connection.prepareStatement(sql)) {
+            ps.setString(1, item.getName());
+            ps.setString(2, item.getDesc());
+            ps.setTimestamp(3, new Timestamp(item.getCreated()));
+            ps.setInt(4, Integer.parseInt(id));
+            ps.execute();
+        } catch (SQLException e) {
+            LOG.catching(e);
+        }
         return false;
     }
 
     @Override
     public boolean delete(String id) {
-        return false;
+        boolean res = false;
+        if (id.length() > 0) {
+            try (PreparedStatement ps = this.connection.prepareStatement("delete from items where id=?")) {
+                ps.setInt(1, Integer.parseInt(id));
+                ps.execute();
+                res = true;
+            } catch (SQLException e) {
+                LOG.catching(e);
+            }
+        }
+        return res;
     }
 
     @Override
     public List<Item> findAll() {
-        return null;
+        List<Item> list = new LinkedList<>();
+        String sql = "select * from items";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Item item = getItemByResultSet(rs);
+                list.add(item);
+            }
+        } catch (SQLException e) {
+            LOG.catching(e);
+        }
+        return list;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        return null;
+        List<Item> list = new LinkedList<>();
+        String sql = "select * from items where name=?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, key);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Item item = getItemByResultSet(rs);
+                list.add(item);
+            }
+
+        } catch (SQLException e) {
+            LOG.catching(e);
+        }
+        return list;
     }
 
     @Override
     public Item findById(String id) {
-        return null;
+        Item item = null;
+        try (PreparedStatement ps = connection.prepareStatement("select * from items where id=?")) {
+            ps.setInt(1, Integer.parseInt(id));
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                item = getItemByResultSet(rs);
+            }
+        } catch (SQLException e) {
+            LOG.catching(e);
+        }
+        return item;
+    }
+
+    private Item getItemByResultSet(ResultSet rs) throws SQLException {
+        Item item = new Item();
+        item.setId(String.valueOf(rs.getInt("id")));
+        item.setName(rs.getString("name"));
+        item.setDesc(rs.getString("description"));
+        item.setCreated(rs.getTimestamp("created").getTime());
+        return item;
     }
 }
--- chapter_008/src/test/java/ru/job4j/tracker/TrackerSQLTest.java
+++ chapter_008/src/test/java/ru/job4j/tracker/TrackerSQLTest.java
@@ -2,14 +2,80 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
 public class TrackerSQLTest {
 
+    TrackerSQL sql = new TrackerSQL();
+
     @Test
     public void checkConnection() {
-        TrackerSQL sql = new TrackerSQL();
         assertThat(sql.init(), is(true));
     }
+
+    @Test
+    public void setAndGetOneItem() {
+        sql.init();
+        Item added = sql.add(new Item("Notebook FFi", "Acer 23"));
+        Item found = sql.findById(added.getId());
+        assertThat(found.getName(), is(added.getName()));
+        assertThat(found.getDesc(), is(added.getDesc()));
+        assertTrue(added.equals(found));
+        assertTrue(sql.delete(added.getId()));
+    }
+
+    @Test
+    public void update() {
+        sql.init();
+        Item added = sql.add(new Item("Notebook", "Acer"));
+        added.setName("Laptok");
+        sql.replace(added.getId(), added);
+        Item found = sql.findById(added.getId());
+        assertThat(found.getName(), is(added.getName()));
+        assertThat(found.getDesc(), is(added.getDesc()));
+        assertThat(found.getCreated(), is(added.getCreated()));
+        assertTrue(added.equals(found));
+        assertTrue(sql.delete(added.getId()));
+    }
+
+    @Test
+    public void whenFindByNameTwoItems() {
+        sql.init();
+        Item previous = new Item("test1", "testDescription", 123L);
+        Item second = new Item("test", "testDescription2", 124L);
+        Item third = new Item("test", "testDescription3", 125L);
+        sql.add(previous);
+        sql.add(second);
+        sql.add(third);
+        List<Item> expected = Arrays.asList(second, third);
+        List<Item> res = sql.findByName("test");
+        assertThat(expected, is(res));
+        sql.delete(previous.getId());
+        sql.delete(second.getId());
+        sql.delete(third.getId());
+    }
+
+    @Test
+    public void whenFindAll() {
+        sql.init();
+        Item previous = new Item("test1", "testDescription", 123L);
+        Item second = new Item("test2", "testDescription2", 124L);
+        Item third = new Item("test3", "testDescription3", 125L);
+        sql.add(previous);
+        sql.add(second);
+        sql.add(third);
+        List<Item> expected = Arrays.asList(previous, second, third);
+        List<Item> res = sql.findAll();
+        assertThat(expected, is(res));
+        sql.delete(previous.getId());
+        sql.delete(second.getId());
+        sql.delete(third.getId());
+    }
+
+
 }
