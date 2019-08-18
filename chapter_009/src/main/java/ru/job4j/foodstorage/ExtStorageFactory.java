68e0c251277e7563a18a4027dc55bff7f34c313d
pavelchernyshev <89119945499@yandex.ru>
2019-08-18 20:13:02 +0300

    2. Расширенное хранилище. [#853] Фабрика по определению складов, на этот раз осдает ссылки объектов и ранее созданных в том числе, а не каждый раз новый объект, также умеет отправлять продукты на склад по переработи и учитывает заполненность первого склада, в случае чего подставит второй склад

---
+++ chapter_009/src/main/java/ru/job4j/foodstorage/ExtStorageFactory.java
@@ -0,0 +1,49 @@
+package ru.job4j.foodstorage;
+
+
+public class ExtStorageFactory implements IStorageFactory {
+    FoodInspector foodInspector;
+    WareHouse wareHouse;
+    WareHouse wareHouse2;
+    Recycle recycle;
+    ColdPlaceStorageDecorator coldPlaceStorageDecorator;
+    Trash trash;
+    Shop shop;
+
+    public ExtStorageFactory() {
+        this.foodInspector  = new FoodInspector();
+        this.wareHouse  = new WareHouse("Stock spb");
+        this.wareHouse2  = new WareHouse("Stock spb 2");
+        this.recycle  = new Recycle("center of recycling");
+        this.coldPlaceStorageDecorator  = new ColdPlaceStorageDecorator(recycle);
+        this.trash  = new Trash("North spb");
+        this.shop  = new Shop("Okey");
+    }
+
+    @Override
+    public IStorage getStorage(IFood food) {
+        IStorage storage;
+        int expiredPercent = foodInspector.getExpirePercent(food);
+        if (expiredPercent == 100) {
+            if (food.canReproduct() && food.isVegetable()) {
+                storage = coldPlaceStorageDecorator;
+            } else if (food.canReproduct()) {
+                storage = recycle;
+            } else {
+                storage = trash;
+            }
+        } else if (expiredPercent > 75) {
+            food.setDiscount(33.33d);
+            storage = shop;
+        } else if (expiredPercent >= 25) {
+            storage = shop;
+        } else {
+            if (wareHouse.getFoodList().size() < 1) {
+                storage = wareHouse;
+            } else {
+                storage = wareHouse2;
+            }
+        }
+        return storage;
+    }
+}
